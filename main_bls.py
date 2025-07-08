713167144cfd5087a4f5c83c1701506508747ad1
smguri002 <57817751+smguri002@users.noreply.github.com>
2025-07-08 19:43:06 -0400

    new CSV up to May

--- main_bls.py
+++ main_bls.py
@@ -24,18 +24,18 @@ BLS_API_KEY = df_ky['BLS_API'][0]
 #bls_dt = labor_US(BLS_API_KEY, 'POWER DELIVER SG.CSV' , ['IPUCN2211__T051000000', 'IPUCN2211__T011000000'], 2000, 2022 )
 #CPI for Gas, Groceries, necessities. 
 #CPI-->CUSR0000SA0 ALL URBAN AREAS
-bls_dt = CPI_Puller(BLS_API_KEY, 'CPI_2019-MTD.csv',
+bls_dt = CPI_Puller(BLS_API_KEY, 'CPI_2010-Current.csv',
                         ['CUSR0000SA0', 'CUSR0000SETB01', 'CUSR0000SAF1', 'CUSR0000SETA02']
-                        , 2019, 2025)
+                        , 2010, 2025)
 Power_Delivery = Power_Delivery(BLS_API_KEY, 'POWER_SECTOR_OUTPUT.csv'
                                 ,['IPUCN2211__T051000000', 'IPUCN2211__T011000000']
-                                , 2006, 2025 )
+                                , 2010, 2025 )
 # Labor Civilian Workforce code: LNS11000000, Employment code: LNS12000000
 # 16 years and older and under 65 years old all civilians
 #Pay and Benefits NBU20530000000000033030
-Labor = US_Labor_Force(BLS_API_KEY, 'Labor_Force_2006-2025.csv'
+Labor = US_Labor_Force(BLS_API_KEY, 'Labor_Force_2010-Current.csv'
                                 ,['LNS11000000']
-                                , 2006, 2025 )
+                                , 2010, 2025 )
 #PCU321991321991  Manufactured home, mobile home, manufacturing
-Lumber = US_Lumber(BLS_API_KEY, 'LUMBER_HOUSES.CSV',
-                ['PCU321991321991'], 2006, 2025)
+Lumber = US_Lumber(BLS_API_KEY, 'LUMBER_HOUSES_2010-Current.csv',
+                ['PCU321991321991'], 2010, 2026)
