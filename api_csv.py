b0775419da8e559f23c5875705c698299da4cccc
GDS Solutions <sguri003@outlook.com>
2025-07-25 00:41:46 +0000

    Added June data

--- api_csv.py
+++ api_csv.py
@@ -2,8 +2,10 @@ import csv
 import pandas as pd         
 import numpy as np  
 #altering  dd
-df_ky = pd.read_csv('BLS_API_KEY.csv')
-print(df_ky['API'][0])
+df_ky = pd.read_csv('API_KEY.csv')
+print(df_ky['BLS_API'][0])
 print(type(df_ky))
 print(dict(df_ky).items)
 dc = dict(df_ky)
+df_api = pd.DataFrame(df_ky)
+print(df_api)
