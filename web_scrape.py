24fda7183faca9bd2745604286b6cdc1a21ec52e
smguri002 <57817751+smguri002@users.noreply.github.com>
2025-08-31 21:13:14 -0400

    adding endpoint json

--- web_scrape.py
+++ web_scrape.py
@@ -4,6 +4,5 @@ import requests
 fin_data = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo")
 print(fin_data)
 source = fin_data.text
-
 data = json.loads(source)
 print(data)
