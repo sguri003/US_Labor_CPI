106896b212695e8dc03361e65562b1a539af98a8
smguri002 <57817751+smguri002@users.noreply.github.com>
2025-08-31 22:08:55 -0400

    add regex

--- web_scrape.py
+++ web_scrape.py
@@ -1,8 +1,19 @@
 import json
 import requests
-
+import re 
 fin_data = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo")
-print(fin_data)
-source = fin_data.text
-data = json.loads(source)
-print(data)
+#print(fin_data)
+source = fin_data.json()
+print(type(source))
+#print(source.keys())
+#print(source['Meta Data'])
+for k, v in source['Time Series (Daily)'].items():
+    #print(f"Key: {k}")
+    #print(f"{v}")
+    if re.search("\d+[.]\d+" , str(v)):
+        print(v)
+    else:
+        break
+    
+
+
