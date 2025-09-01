e9ec6acf7f6bba051140ed66d131929c648b231d
smguri002 <57817751+smguri002@users.noreply.github.com>
2025-08-31 21:07:20 -0400

    adding financial end point

--- web_scrape.py
+++ web_scrape.py
@@ -1,9 +1,9 @@
 import json
 import requests
-import seaborn
 
-with requests("'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo") as response:
-    source = response.read()
+fin_data = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo")
+print(fin_data)
+source = fin_data.text
 
 data = json.loads(source)
 print(data)
